5b2993ca3637af3cc30a000d0cc6c7d880a88700
Michael <fauxpawsc@live.com>
2016-02-17 19:30:10 -0800

    stage before server and client assimilation

--- client/scripts/gameApp.js
+++ client/scripts/gameApp.js
@@ -1,17 +1,8 @@
-//Initialize canvas
-var canvas = document.createElement('canvas');
-var ctx = canvas.getContext('2d');
-canvas.width = 640;
-canvas.height = 480;
-document.body.appendChild(canvas);
 
-//create all global variables
 var lastTime;
 var gameTime = 0;
-var appWidth = canvas.getAttribute('width');
-var appHeight = canvas.getAttribute('height');
-var mouseX;
-var mouseY;
+// var appWidth = canvas.getAttribute('width');
+// var appHeight = canvas.getAttribute('height');
 var attackButton = {
   x:195,
   y:385,
@@ -21,133 +12,162 @@ var attackButton = {
 
 var hero = {
   name:'Charlie',
-  hp: 9999
+  hp: 2000
 };
 
 var enemy = {
   name:'bug',
-  hp: 9999
+  hp: 1000
 };
 
-//TODO wrap this in document loader function
-var background = new Image();
-var monster = new Image();
-var heroStats = new Image();
-var battleOptions = new Image();
-var shield = new Image();
-heroStats.src = 'images/heroStatus.png';
-monster.src = 'images/monster0.png';
-background.src = 'images/bg.png';
-battleOptions.src = 'images/battleOptions.png';
-shield.src = 'images/shield.png';
+function battleState(){
+  clearCanvas(ctx);
 
-ctx.drawImage(background,0,0);
-ctx.drawImage(monster,282,140);
-ctx.drawImage(heroStats,5,375);
-ctx.drawImage(battleOptions, 155, 375);
-ctx.drawImage(shield, 165, 375);
+  var background = new Image();
+  var monster = new Image();
+  var heroStats = new Image();
+  var battleOptions = new Image();
+  var shield = new Image();
+  heroStats.src = 'images/heroStatus.png';
+  monster.src = 'images/monster0.png';
+  background.src = 'images/bg.png';
+  battleOptions.src = 'images/battleOptions.png';
+  shield.src = 'images/shield.png';
 
-//text stuff for heroStatus
-ctx.font = '14px Arial';
-ctx.fillStyle = 'white';
-ctx.fillText('Hero Name',10,390);
-ctx.fillText('HP:',10,420);
+  background.onload = function(){
+    ctx.drawImage(background,0,0);
+  };
+  monster.onload = function(){
+    ctx.drawImage(monster,282,140);
+  };
+  heroStats.onload = function(){
+    ctx.drawImage(heroStats,5,375);
+  };
+  battleOptions.onload = function(){
+    ctx.drawImage(battleOptions, 155, 375);
+  };
+  // shield.onload = function(){
+  //   ctx.drawImage(shield, 165, 375);
+  // };
 
-//text stuff for battleOptions
-ctx.fillText('Attack', 200, 395);
+  canvas.addEventListener('mousemove', checkPosBattle);
 
-canvas.addEventListener('mousemove', checkPos);
-
-function checkPos(mouseEvent){
-  mouseX = mouseEvent.pageX - this.offsetLeft;
-  mouseY = mouseEvent.pageY - this.offsetTop;
-  // console.log(mouseX);
-  // console.log(mouseY);
-};
-function heroAttack(){
-  var dmg = Math.floor((Math.random() * 100) + 100);
-  enemy.hp -= dmg;
-  console.log('new monster hp is ' + enemy.hp);
-  console.log(dmg);
-  return dmg;
-}
-
-function monsterAttack(){
-  var dmg = Math.floor((Math.random() * 100) + 100);
-  hero.hp -= dmg;
-  console.log('monster does ' + dmg);
-  console.log('your new hp is ' + hero.hp);
-  canvas.addEventListener('mouseup', checkClick);
-  return dmg;
-};
-
-
-canvas.addEventListener('mouseup', checkClick);
-
-function checkClick(dmg){
-  if(mouseX > attackButton.x && mouseX < attackButton.x + attackButton.width){
-    if(mouseY > attackButton.y && mouseY < attackButton.y + attackButton.height){
-      dmg = heroAttack();
-      console.log('you did ' + dmg + ' dmg!');
-      console.log('you clicked attack!');
-      canvas.removeEventListener('mouseup', checkClick);
-      monsterAttack();
-
-    }
+  function checkPosBattle(mouseEvent){
+    mouseX = mouseEvent.pageX - this.offsetLeft;
+    mouseY = mouseEvent.pageY - this.offsetTop;
+    // console.log(mouseX);
+    // console.log(mouseY);
+  };
+  function heroAttack(){
+    var dmg = Math.floor((Math.random() * 100) + 100);
+    enemy.hp -= dmg;
+    console.log('new monster hp is ' + enemy.hp);
+    console.log(dmg);
+    return dmg;
   }
+
+  function monsterAttack(){
+    var dmg = Math.floor((Math.random() * 100) + 100);
+    hero.hp -= dmg;
+    console.log('monster does ' + dmg);
+    console.log('your new hp is ' + hero.hp);
+    canvas.addEventListener('mouseup', checkClickBattle);
+    return dmg;
+  };
+
+  canvas.addEventListener('mouseup', checkClickBattle);
+
+  function checkClickBattle(dmg){
+    if(mouseX > attackButton.x && mouseX < attackButton.x + attackButton.width){
+      if(mouseY > attackButton.y && mouseY < attackButton.y + attackButton.height){
+        dmg = heroAttack();
+        console.log('you did ' + dmg + ' dmg!');
+        console.log('you clicked attack!');
+        canvas.removeEventListener('mouseup', checkClickBattle);
+        monsterAttack();
+        if(enemy.hp <=0) {
+          console.log('You Win!');
+          canvas.removeEventListener('mouseup',checkClick);
+        }
+        else if(hero.hp <=0) {
+          console.log('Game Over');
+          canvas.removeEventListener('mouseup',checkClick);
+        }
+      }
+    }
+  };
+
+  function battleScene(){
+    clearCanvas(ctx);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0,appWidth, appHeight);
+    setTimeout(function(){
+      ctx.drawImage(background,0,0);
+      ctx.drawImage(monster,282,140);
+      ctx.drawImage(heroStats,5,375);
+      ctx.drawImage(battleOptions, 155, 375);
+      ctx.drawImage(shield, 165, 375);
+      //text stuff for heroStatus
+      ctx.font = '14px Arial';
+      ctx.fillStyle = 'white';
+      ctx.fillText('Hero Name',10,390);
+      ctx.fillText('HP:',10,420);
+      //text stuff for battleOptions
+      ctx.fillText('Attack', 200, 395);
+      console.log('transition complete!'); }, 10);
+  }
+  battleScene();
+
 };
+//TODO wrap this in document loader function
 
 
 
 //resource prep
-resources.load([
-  //TODO add sprite sheet paths
-    /*'...png',
-    '...png',
-    '...png'
-    */
-]);
+// resources.load([
+//   //TODO add sprite sheet paths
+//     /*'...png',
+//     '...png',
+//     '...png'
+//     */
+// ]);
 
-resources.onReady(initialize);
+//resources.onReady(initialize);
 
 //main game Loop
-function main (){
-  //create variables for keeping track of change in time between game updates (delta time)
-  var now = Date.now();
-  var dt = (now - lastTime)/1000.0;
-  update(dt);
-  render();
-  lastTime = now;
-  requestAnimFrame();
-
-};
-
-
-function initialize(){
-  //TODO add initial map state
-  lastTime = Date.now;
-  main();
-};
-
-function update(dt){
-  gameTime += dt;
-  checkInput(dt);
-  updateEntities(dt);
-};
-
-function checkInput(dt){
-  //TODO add logic for different keydowns or mouse events
-};
-
-function updateEntities(dt){
-  //TODO add logic to update changes made to sceen or map
-};
-
-//draw everything
-function render(){
-  //TODO add logic to display elements at correct locations
-};
-
-function requestAnimFrame(){
-  //TODO add logic for animation
-}
+// function main (){
+//   //create variables for keeping track of change in time between game updates (delta time)
+//   var now = Date.now();
+//   var dt = (now - lastTime)/1000.0;
+//   update(dt);
+//   render();
+//   lastTime = now;
+//   requestAnimFrame();
+//
+// };
+//
+//
+// function initialize(){
+//   //TODO add initial map state
+//   lastTime = Date.now;
+//   main();
+// };
+//
+// function update(dt){
+//   gameTime += dt;
+//   checkInput(dt);
+//   updateEntities(dt);
+// };
+//
+// function checkInput(dt){
+//   //TODO add logic for different keydowns or mouse events
+// };
+//
+// function updateEntities(dt){
+//   //TODO add logic to update changes made to sceen or map
+// };
+//
+// //draw everything
+// function render(){
+//   //TODO add logic to display elements at correct locations
+// };
--- client/scripts/startMenu.js
+++ client/scripts/startMenu.js
@@ -1,138 +1,153 @@
-//credit to Tyler Seitz for his tutorial on animating game menuShield
-//much of the following code was adapted from his methodologies
-
-if(!canvas){
-  var canvas = document.createElement('canvas');
-  var ctx = canvas.getContext('2d');
-  canvas.width = 640;
-  canvas.height = 480;
-  document.body.appendChild(canvas);
-  var appWidth = canvas.getAttribute('width');
-  var appHeight = canvas.getAttribute('height');
-}
-
-var bgImage = new Image();
-var titleImage = new Image();
-var newGameImage = new Image();
-var loadGame = new Image();
-var creditsImage = new Image();
-var shieldImage = new Image();
-bgImage.src = 'images/menuBG.png';
-titleImage.src = 'images/menuTitle.png';
-newGameImage.src = 'images/newGame.png';
-loadGame.src = 'images/loadGame.png';
-creditsImage.src = 'images/credits.png';
-shieldImage.src = 'images/menuShield.png';
-
-var buttonX = [189,172,203];
-var buttonY = [336,378,421];
-var buttonWidth = [248,283,219];
-var buttonHeight = 29;
+var canvas = document.createElement('canvas');
+var ctx = canvas.getContext('2d');
+canvas.width = 640;
+canvas.height = 480;
+//document.body.appendChild(canvas);
+$('#mycanvas').append(canvas);
+var appWidth = canvas.getAttribute('width');
+var appHeight = canvas.getAttribute('height');
+//global mouse tracker coords
 var mouseX;
 var mouseY;
-var shieldX = [0,0];
-var shieldY = [0,0];
-var shieldWidth = 25;
-var shieldHeight = 29;
-var shieldVis = false;
-var shieldSize = shieldWidth;
-var shieldRotate = 0;
 
-bgImage.onload = function(){
-  ctx.drawImage(bgImage,0,0);
-};
-titleImage.onload = function(){
-  ctx.drawImage(titleImage, 75, 45);
-};
-newGameImage.onload = function(){
-  ctx.drawImage(newGameImage, buttonX[0], buttonY[0]);
-};
-loadGame.onload = function(){
-  ctx.drawImage(loadGame, buttonX[1], buttonY[1]);
-};
-creditsImage.onload = function(){
-  ctx.drawImage(creditsImage, buttonX[2], buttonY[2]);
-};
+(function(){
+  //events for main menu
+  canvas.addEventListener('mousemove', checkPos);
+  canvas.addEventListener('mousedown', checkClick);
+  //button sizes and locations
+  var buttonX = [189,172,203];
+  var buttonY = [336,378,421];
+  var buttonWidth = [248,283,219];
+  var buttonHeight = 29;
+  //codefellows logo animation vars
+  var shieldX = [0,0];
+  var shieldY = [0,0];
+  var shieldWidth = 25;
+  var shieldHeight = 29;
+  var shieldVis = false;
+  var shieldSize = shieldWidth;
+  var shieldRotate = 0;
+  //image prep for drawing to menu scene
+  var bgImage = new Image();
+  var titleImage = new Image();
+  var newGameImage = new Image();
+  var loadGame = new Image();
+  var creditsImage = new Image();
+  var shieldImage = new Image();
 
-canvas.addEventListener('mousemove', checkPos);
-canvas.addEventListener('mousedown', checkClick);
+  bgImage.src = 'images/menu/menuBG.png';
+  bgImage.xcoord = 0;
+  bgImage.ycoord = 0;
+  titleImage.src = 'images/menu/menuTitle.png';
+  titleImage.xcoord = 75;
+  titleImage.ycoord = 45;
+  newGameImage.src = 'images/menu/newGame.png';
+  newGameImage.xcoord = 189;
+  newGameImage.ycoord = 336;
+  loadGame.src = 'images/menu/loadGame.png';
+  loadGame.xcoord = 172;
+  loadGame.ycoord = 378;
+  creditsImage.src = 'images/menu/credits.png';
+  creditsImage.xcoord = 203;
+  creditsImage.ycoord = 421;
+  shieldImage.src = 'images/menu/menuShield.png';
 
-var frames = 30;
-var timerId = 0;
+  bgImage.onload = function(){
+    ctx.drawImage(bgImage, bgImage.xcoord, bgImage.ycoord);
+  };
+  titleImage.onload = function(){
+    ctx.drawImage(titleImage, titleImage.xcoord, titleImage.ycoord);
+  };
+  newGameImage.onload = function(){
+    ctx.drawImage(newGameImage, newGameImage.xcoord, newGameImage.ycoord);
+  };
+  loadGame.onload = function(){
+    ctx.drawImage(loadGame, buttonX[1], buttonY[1]);
+  };
+  creditsImage.onload = function(){
+    ctx.drawImage(creditsImage, creditsImage.xcoord, creditsImage.ycoord);
+  };
 
-timerId = setInterval(update, 1000/frames);
-
-function update(){
-  clearApp();
-  move();
-  draw();
-}
-
-function move(){
-  if(shieldSize == shieldWidth){
-    shieldRotate = -1;
-  }
-  if(shieldSize == 0){
-    shieldRotate = 1;
-  }
-  shieldSize += shieldRotate;
-
-}
-function draw(){
-  ctx.drawImage(bgImage,0,0);
-  ctx.drawImage(titleImage, 75, 45);
-  ctx.drawImage(newGameImage, buttonX[0], buttonY[0]);
-  ctx.drawImage(loadGame, buttonX[1], buttonY[1]);
-  ctx.drawImage(creditsImage, buttonX[2], buttonY[2]);
-  if(shieldVis === true){
-    ctx.drawImage(shieldImage, shieldX[0] - (shieldSize/2), shieldY[0], shieldSize, shieldHeight );
-    ctx.drawImage(shieldImage, shieldX[1] - (shieldSize/2), shieldY[1], shieldSize, shieldHeight );
-  }
-}
-function clearApp(){
-  ctx.clearRect(0, 0, appWidth, appHeight);
-}
-
-function checkPos(mouseEvent){
-  mouseX = mouseEvent.pageX - this.offsetLeft;
-  mouseY = mouseEvent.pageY - this.offsetTop;
-
-  for (i = 0; i < buttonX.length; i++){
-    if(mouseX > buttonX[i] && mouseX < buttonX[i] + buttonWidth[i]){
-      if(mouseY > buttonY[i] && mouseY < buttonY[i] + buttonHeight){
-        console.log('(' + mouseX + ', ' + mouseY + ')');
-        shieldVis = true;
-        shieldX[0] = buttonX[i] - (shieldWidth);
-        shieldY[0] = buttonY[i];
-        shieldX[1] = buttonX[i] + buttonWidth[i] + (shieldWidth);
-        shieldY[1] = buttonY[i];
-      }
-    }else{
-      shieldVis = false;
+  function draw(){
+    ctx.drawImage(bgImage, bgImage.xcoord, bgImage.ycoord);
+    ctx.drawImage(titleImage, titleImage.xcoord, titleImage.ycoord);
+    ctx.drawImage(newGameImage, newGameImage.xcoord, newGameImage.ycoord);
+    ctx.drawImage(loadGame, loadGame.xcoord, loadGame.ycoord);
+    ctx.drawImage(creditsImage, creditsImage.xcoord, creditsImage.ycoord);
+    if(shieldVis === true){
+      ctx.drawImage(shieldImage, shieldX[0] - (shieldSize/2), shieldY[0], shieldSize, shieldHeight);
+      ctx.drawImage(shieldImage, shieldX[1] - (shieldSize/2), shieldY[1], shieldSize, shieldHeight);
     }
   }
-};
+  var frames = 30;
+  var timerId = 0;
 
-function checkClick(){
-  console.log('x: ' + mouseX);
-  console.log('y: ' + mouseY);
-  var fadeId = 0;
-  for (i=0; i < buttonX.length; i++){
-    if(mouseX > buttonX[i] && mouseX < buttonX[i] + buttonWidth[i]){
-      if(mouseY > buttonY[i] && mouseY < buttonY[i] + buttonHeight){
-        fadeId = setInterval('fadeOut()', 1000/frames);
-        clearInterval(timerId);
-        canvas.removeEventListener('mousemove', checkPos);
-        canvas.removeEventListener('mousedown', checkClick);
+  timerId = setInterval(update, 1000/frames);
 
+  function update(){
+    clearApp();
+    move();
+    draw();
+  }
+
+  function move(){
+    if(shieldSize == shieldWidth){
+      shieldRotate = -1;
+    }
+    if(shieldSize == 0){
+      shieldRotate = 1;
+    }
+    shieldSize += shieldRotate;
+
+  }
+
+  function clearApp(){
+    ctx.clearRect(0, 0, appWidth, appHeight);
+  }
+
+  function checkPos(mouseEvent){
+    mouseX = mouseEvent.pageX - this.offsetLeft;
+    mouseY = mouseEvent.pageY - this.offsetTop;
+
+    for (i = 0; i < buttonX.length; i++){
+      if(mouseX > buttonX[i] && mouseX < buttonX[i] + buttonWidth[i]){
+        if(mouseY > buttonY[i] && mouseY < buttonY[i] + buttonHeight){
+          console.log('(' + mouseX + ', ' + mouseY + ')');
+          shieldVis = true;
+          shieldX[0] = buttonX[i] - (shieldWidth);
+          shieldY[0] = buttonY[i];
+          shieldX[1] = buttonX[i] + buttonWidth[i] + (shieldWidth);
+          shieldY[1] = buttonY[i];
+        }
+      }else{
+        shieldVis = false;
+      }
+    }
+  };
+
+  function checkClick(){
+    console.log('x: ' + mouseX);
+    console.log('y: ' + mouseY);
+    var fadeId = 0;
+    for (i=0; i < buttonX.length; i++){
+      if(mouseX > buttonX[i] && mouseX < buttonX[i] + buttonWidth[i]){
+        if(mouseY > buttonY[i] && mouseY < buttonY[i] + buttonHeight){
+          canvas.removeEventListener('mousemove', checkPos);
+          canvas.removeEventListener('mousedown', checkClick);
+          clearInterval(timerId);
+          fadeOut();
+        };
       };
     };
   };
-};
+})();
 
 function fadeOut(){
   setTimeout(function(){
-    ctx.fillStyle = 'rgba(0,0,0,0.2)';
+    ctx.fillStyle = 'rgba(0,0,0,0.1)';
     ctx.fillRect(0, 0, appWidth, appHeight);
-    main(); }, 500);
+    //DONE call function for button click from menu
+    battleState();
+    console.log('porting!');}, 500);
+
 }
