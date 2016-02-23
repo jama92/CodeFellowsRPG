33aed3c9d2daaf6a38300fdd50a29fa1e608ad22
jama92 <andrewmceniry@gmail.com>
2016-02-22 20:50:55 -0800

    commit

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   pokeStart: 1, // Sets starting pokedex id
   pokeEnd: 15, // Number of pokemon to grab from the api
-  randomTypes: ['grass', 'fire', 'water'], // array of types the random function grabs from
+  randomTypes: ['grass', 'fire', 'water'], // array of types the random function grabs from /getRandom
   queryQty: 3 // Number of Pokemon returned for each type, per query
 };
